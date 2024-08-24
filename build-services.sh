6ab273afe50327f9d89d17a0d4159338b20677fd
binu balan <binu.remote.360@gmail.com>
2024-08-25 01:47:05 +0530

    Revise docker compose build up

--- build-services.sh
+++ build-services.sh
@@ -1,7 +1,7 @@
 
 # pre-build base image
-docker build -t  pelv-rhel-go-base-image ./pelv-rhel-go-base-image --no-cache
+docker build -t  pelv-rhel-go-base-image ./pelv-rhel-go-base-image # --no-cache
 
 #  build the microservices
-# docker-compose up --build --no-cache
-docker-compose build --no-cache
+docker-compose up --build # --no-cache
+# docker-compose build --no-cache
